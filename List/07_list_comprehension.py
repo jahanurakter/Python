5856c314c0bf2401ddc606ac45ebdf7171065f56
Md Arafat Hossain <jahant.tech@gmail.com>
2026-08-11 10:06:17 +0600

    update

---
+++ List/07_list_comprehension.py
@@ -0,0 +1,6 @@
+#old list filter kore new list make kora list comprehension er kaj
+#for_list er shortcut
+
+fruits = ["aplle","Banana","Cherry"]
+fruit= [x for x in fruits if "a" in x]
+print(fruit)
