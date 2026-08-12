a50ab367a5404c05c2ab1f7196753aab1c02dba4
Maya Rahman <jahant.tech@gmail.com>
2026-08-12 10:43:10 +0600

    up

--- Function_/06_docstring.py
+++ Function_/06_docstring.py
@@ -4,14 +4,14 @@
     This will take two parameter        #function er bahire ti print korbe na
     Return value is integer
 '''
-
+x = 10                  #global variable
 
 def Add(a, b):
     '''
     This will take two parameter            #def er modde multiline comment thakay ata show korbe
     Return value is integer
     '''
-    return a + b
+    return a + b                #local variable
 
 
 # print(Add(10, 20))
--- Function_/08_map_function.py
+++ Function_/08_map_function.py
@@ -3,3 +3,13 @@ def myfunc(a):
 
 x = map(myfunc, ('apple', 'banana', 'cherry'))    #map - loop er moto kaj kore
 print(list(x))              #list na likhle print hbe na
+
+
+def multiply(a, b):
+    return b * 2
+
+numbers1 = [1, 2, 3]
+numbers2 = [4, 5, 6]
+
+result = map(multiply, numbers1, numbers2)      #map choose (function name and value)
+print(list(result))
