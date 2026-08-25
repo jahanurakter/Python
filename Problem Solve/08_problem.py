48cf2e9884f7b1d383542defcf3d604c1f7971ca
Maya Rahman <jahant.tech@gmail.com>
2026-08-25 09:23:03 +0600

    up

--- Problem Solve/08_problem.py
+++ Problem Solve/08_problem.py
@@ -4,3 +4,11 @@ x=5
 
 for i in range(1,11):
     print(f"{x}X{i}={x*i}")
+#-------------------------------------
+'''Read a number from input.
+Print its multiplication table from 1 to 10.'''
+
+n = int(input("Enter your number: "))
+
+for i in range(1,11):
+    print(f"{n}X{i}={n*i}")
